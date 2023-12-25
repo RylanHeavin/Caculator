644d0cb3d0c0fc75548ed2f00025b4a8e4105f83
Rylan Heavin <rylanheavin@gmail.com>
2023-12-25 14:23:13 -0600

    added comments to the event listeners

--- calculator.js
+++ calculator.js
@@ -1,11 +1,14 @@
+// rename of calculator input
 text = document.getElementById('text')
 
+// adds event listeners to the numbers
 for (let i = 0; i<10; i++) {
     document.getElementById(i.toString()).addEventListener('click', () => {
         text.value += document.getElementById(i.toString()).innerText
     })
 }
 
+// adds event listeners to the symbols
 symbols = ["/", "x", "-", "+", ".", "(", ")"]
 symbols.forEach(symbol => {
     document.getElementById(symbol).addEventListener('click', () => {
@@ -13,12 +16,14 @@ symbols.forEach(symbol => {
 })
 })
 
+// checks if the input only holds the correct numbers or symbols
 text.addEventListener("input", () => {
     if (!(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "/", "x", "-", "+", "."].includes(text.value[text.value.length-1]))) {
         text.value = text.value.slice(0, text.value.length-1)
     }
 })
 
+// actaul logic for calcuator like 1+1
 document.getElementById('=').addEventListener('click', () => {
     let string = text.value
     let new_value = 0
@@ -26,7 +31,9 @@ document.getElementById('=').addEventListener('click', () => {
         window.prompt(string.slice(string.indexOf('+')-1, string.indexOf('+')+2))
         while (string.includes("+")) {
             window.prompt(string.slice(string.indexOf('+')-1, string.indexOf('+')+2))
+            
             new_value = Number(string[string.indexOf('+')-1]) + Number(string[string.indexOf('+')+2])
+            
             string = string.slice(0, string.indexOf('+')-2) + new_value + string.slice(string.indexOf('+')+3)
         }
     } catch(e) {
@@ -35,6 +42,7 @@ document.getElementById('=').addEventListener('click', () => {
     text.value = string
 })
 
+// adds functionality to the clear button
 document.getElementById('c').addEventListener('click', () => {
     text.value = ""
 })
